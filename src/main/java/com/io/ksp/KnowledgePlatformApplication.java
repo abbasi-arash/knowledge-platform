b16e265a9aef55210792ff8941584089ac2df1b8
arash <arash1abbasi@gmail.com>
2022-07-05 00:00:01 +0400

    add swagger

--- src/main/java/com/io/ksp/KnowledgePlatformApplication.java
+++ src/main/java/com/io/ksp/KnowledgePlatformApplication.java
@@ -2,8 +2,10 @@ package com.io.ksp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class KnowledgePlatformApplication {
 
     public static void main(String[] args) {
