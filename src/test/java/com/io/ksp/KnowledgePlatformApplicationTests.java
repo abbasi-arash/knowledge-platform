69d2af4e01415001b622ecc19d671353bd588cae
arash <arash1abbasi@gmail.com>
2022-07-04 19:10:47 +0400

    first commit

---
+++ src/test/java/com/io/ksp/KnowledgePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.ksp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
